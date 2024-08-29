f863705e2b92cb0fee8fbe2d0e6f8af3551b3844
Leo <dev.rignaneseleo@gmail.com>
2024-08-29 12:21:00 +0200

    assets gen

---
+++ my_template/lib/core/gen/assets.gen.dart
@@ -0,0 +1,24 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsI18nGen {
+  const $AssetsI18nGen();
+
+  /// File path: assets/i18n/en.json
+  String get en => 'assets/i18n/en.json';
+
+  /// List of all assets
+  List<String> get values => [en];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsI18nGen i18n = $AssetsI18nGen();
+}
