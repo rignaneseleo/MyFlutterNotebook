b22a86410d6227e732309f5fc3f2dfc97dc695d6
Leo <dev.rignaneseleo@gmail.com>
2025-04-27 18:26:42 +0200

    Update authorized_page.dart

--- my_template/lib/core/router/pages/authorized_page.dart
+++ my_template/lib/core/router/pages/authorized_page.dart
@@ -1,9 +1,10 @@
+/*
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 // This page is used to nest the authorized routes
 // https://pub.dev/packages/auto_route#nested-navigation
-/*
+
 @RoutePage()
 class AuthorizedPage extends StatelessWidget {
   const AuthorizedPage({super.key});
